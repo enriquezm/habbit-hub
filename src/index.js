aed5145e1c0edd0b859af5d0f746d6f3591ecdef
Myles Enriquez <enri.myles@gmail.com>
2019-01-11 15:45:26 -0800

    Move app to its own component.

--- src/index.js
+++ src/index.js
@@ -1,86 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import Challenge from "./components/Challenge";
-
-import "./index.css";
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      challenges: [
-        {
-          name: "Something",
-          description:
-            "A sample 100days challenge to start you off. Remove me if you like."
-        }
-      ],
-      inputValueName: "",
-      inputValueDesc: ""
-    };
-    this.addChallenge = this.addChallenge.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-  addChallenge(challengeName) {
-    let newChallengeList = [...this.state.challenges];
-    newChallengeList.push({
-      name: this.state.inputValueName,
-      description: this.state.inputValueDesc
-    });
-    this.setState({
-      inputValueName: "",
-      inputValueDesc: "",
-      challenges: newChallengeList
-    });
-  }
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="challenges-header">
-          <h1>HabbitRabbit</h1>
-          <p>Start small. Finish big. Build those habits with habbitRabbit!</p>
-        </div>
-        <div className="challenges-form">
-          <h2>Why not add a challenge?</h2>
-          <label for="inputValueName">#100DaysOf</label>
-          <input
-            value={this.state.inputValueName}
-            id="inputValueName"
-            name="inputValueName"
-            onChange={this.handleChange}
-            type="text"
-          />
-          <label for ="inputValueDesc">Description</label>
-          <input
-            value={this.state.inputValueDesc}
-            id="inputValueDesc"
-            name="inputValueDesc"
-            onChange={this.handleChange}
-            type="text"
-          />
-          <button onClick={this.addChallenge}>Add Challenge</button>
-        </div>
-        <div className="challenges-container">
-          {this.state.challenges.map((challenge, index) => {
-            return (
-              <Challenge
-                key={index}
-                name={challenge.name}
-                description={challenge.description}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
