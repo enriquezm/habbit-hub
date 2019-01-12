30c8c92f416e7cde5445e99521e9c04015da6a7b
Myles Enriquez <enri.myles@gmail.com>
2019-01-11 16:14:50 -0800

    Set up login screen routing.

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => (
+  <div>
+    <h1>Login page</h1>
+    <p>This will be the login page and will.</p>
+    <Header />
+  </div>
+)
+
+export default Login;
